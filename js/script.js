51e090949bc271010f8278734a813a9d6ef0cd4f
9Pu-nex <155422515+9Pu-nex@users.noreply.github.com>
2024-01-06 12:06:23 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menu.addEventListener("click",function(){
+    navbar.classList.toggle("active");
+
+
+});
+
+window.onscroll = () => {
+    navbar.classList.remove("active");
+};
